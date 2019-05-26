3a96b8ef379e5dbed68da4aa64b6a63a257ae8b6
Magnus Møller <magnus108@me.com>
2019-05-26 23:02:30 +0000

    namnam

--- src/Utils/ListZipper.hs
+++ src/Utils/ListZipper.hs
@@ -5,8 +5,11 @@ module Utils.ListZipper
     , rights
     , lefts
     , mapFocus
+    , back
+    , forward
     ) where
 
+import Data.Maybe
 
 import Utils.Comonad
 import Data.Aeson.TH (deriveJSON, defaultOptions)
@@ -17,13 +20,20 @@ deriveJSON defaultOptions ''ListZipper
 
 
 back :: ListZipper a -> ListZipper a
-back (ListZipper (l:ls) a rs) = ListZipper ls l (a:rs)
-back (ListZipper [] a rs) = ListZipper [] a rs
+back a = fromMaybe a (back' a)
+
+back' :: ListZipper a -> Maybe (ListZipper a)
+back' (ListZipper (l:ls) a rs) = Just (ListZipper ls l (a:rs))
+back' (ListZipper [] _ _) = Nothing
 
 
 forward :: ListZipper a -> ListZipper a
-forward (ListZipper ls a (r:rs)) = ListZipper (a:ls) r rs
-forward (ListZipper ls a []) = ListZipper ls a []
+forward a = fromMaybe a (forward' a)
+
+
+forward' :: ListZipper a -> Maybe (ListZipper a)
+forward' (ListZipper ls a (r:rs)) = Just (ListZipper (a:ls) r rs)
+forward' (ListZipper _ _ []) = Nothing
 
 
 lefts :: ListZipper a -> [a]
@@ -42,11 +52,18 @@ mapFocus :: (a -> a) -> ListZipper a -> ListZipper a
 mapFocus f (ListZipper ls a xs) = ListZipper ls (f a) xs
 
 
+iterate' :: ( a -> Maybe a) -> a -> [a]
+iterate' f x = 
+    case f x of
+            Just x' -> x : (iterate' f x')
+            Nothing -> [x]
+
+
 instance Functor ListZipper where
     fmap f (ListZipper ls a rs) = ListZipper (fmap f ls) (f a) (fmap f rs)
 
-
 instance Comonad ListZipper where
     extract (ListZipper _ a _) = a
-    duplicate a = ListZipper (shift back) a (shift forward)
-        where shift move = tail $ iterate move a
+    duplicate a = ListZipper (shift back') a (shift forward')
+        where shift move = tail $ iterate' move a
+
