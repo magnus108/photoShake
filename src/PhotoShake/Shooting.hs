95e2847b2119f7b560577ad09909faf9b93fa4ed
Magnus Møller <magnus108@me.com>
2019-05-26 21:04:29 +0000

    HAJAJ

--- src/PhotoShake/Shooting.hs
+++ src/PhotoShake/Shooting.hs
@@ -2,8 +2,10 @@
 module PhotoShake.Shooting
     ( Shooting(..)
     , Shootings(..)
+    , encode 
     ) where
 
+import Data.Aeson (encode)
 import Data.Aeson.TH (deriveJSON, defaultOptions)
 
 import Utils.ListZipper
