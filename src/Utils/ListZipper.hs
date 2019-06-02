c1ce915c35cac61e3aeb580e9dcc2cf69f5c217d
Magnus Møller <magnus108@me.com>
2019-06-02 18:31:34 +0000

    yoyo

--- src/Utils/ListZipper.hs
+++ src/Utils/ListZipper.hs
@@ -61,7 +61,7 @@ iterate' f x =
 
 
 toList :: ListZipper a -> [a]
-toList (ListZipper ls x rs) = ls ++ (x : rs)
+toList (ListZipper ls x rs) = (reverse ls) ++ (x : rs)
 
 instance Functor ListZipper where
     fmap f (ListZipper ls a rs) = ListZipper (fmap f ls) (f a) (fmap f rs)
