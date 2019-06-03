a161adf920f764028fc56024fb97a135a7587401
Magnus Møller <magnus108@me.com>
2019-06-03 21:02:26 +0000

    ggggg

--- src/PhotoShake/Dump.hs
+++ src/PhotoShake/Dump.hs
@@ -6,8 +6,9 @@ module PhotoShake.Dump
 
 import Data.Aeson.TH (deriveJSON, defaultOptions)
 
-data Dump = Dump { unDump :: FilePath }
-    deriving (Show, Eq)
+data Dump = Dump { unDump :: FilePath } 
+          | NoDump
+                deriving (Show, Eq)
 
 
 deriveJSON defaultOptions ''Dump
