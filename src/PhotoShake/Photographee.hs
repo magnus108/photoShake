37dc4e77617135b2ef5a1cad953276e96276e207
Magnus Møller <magnus108@me.com>
2019-06-19 11:51:03 +0000

    ggg

--- src/PhotoShake/Photographee.hs
+++ src/PhotoShake/Photographee.hs
@@ -7,10 +7,14 @@ module PhotoShake.Photographee
     , insertPhotographee
     ) where
 
+
+import Prelude hiding ((++))
+
 import GHC.Generics (Generic)
 import Data.Csv
-import Data.Vector (find)
+import Data.Vector (Vector, find, (++), fromList, toList)
 import Data.Char
+import qualified Data.ByteString.Lazy as BL
 import Data.ByteString.Lazy.UTF8 (fromString)
 
 import System.FilePath
@@ -42,13 +46,34 @@ instance ToRecord Photographee
 
 
 
-myOptions :: DecodeOptions
-myOptions = defaultDecodeOptions { decDelimiter = fromIntegral (ord ';') }
+myOptionsDecode :: DecodeOptions
+myOptionsDecode = defaultDecodeOptions { decDelimiter = fromIntegral (ord ';') }
+
+myOptionsEncode :: EncodeOptions
+myOptionsEncode = defaultEncodeOptions { encDelimiter = fromIntegral (ord ';') }
+
 
 
 insertPhotographee :: FilePath -> Ident -> String -> FullName -> IO ()
-insertPhotographee location photographeeId grade name =
-    return ()
+insertPhotographee location photographeeId grade name = do
+    -- badness
+    let ext = takeExtension location
+    _ <- case ext of
+            ".csv" -> return ()
+            _ -> throw BadCsv
+
+    locationData' <- readFile location `catchAny` (\_ -> throw LocationConfigFileMissing)
+    seq (length locationData') (return ())
+
+    let locationData = decodeWith myOptionsDecode NoHeader $ fromString locationData' :: Either String (Vector Photographee)
+
+    let studentData = case locationData of
+            Left _ -> throw ParseLocationFile
+            Right locData -> locData ++ (fromList [Photographee "xxx" grade name photographeeId])
+
+    let moreData = encodeWith myOptionsEncode $ toList studentData --can throw error
+
+    BL.writeFile location moreData
 
 findPhotographee :: FilePath -> Ident -> IO Photographee
 findPhotographee location photographeeId = do
@@ -61,7 +86,7 @@ findPhotographee location photographeeId = do
 
     locationData' <- readFile location `catchAny` (\_ -> throw ReadLocationFile)
 
-    let locationData = decodeWith myOptions NoHeader $ fromString locationData'
+    let locationData = decodeWith myOptionsDecode NoHeader $ fromString locationData'
     --could use some case of here and error handling
     let studentData = case locationData of
             Left _ -> throw ParseLocationFile
