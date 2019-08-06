b288213a0e255b6925321f9c440fa7723ac77deb
Magnus Møller <magnus108@me.com>
2019-08-06 08:40:45 +0000

    jdada

--- src/PhotoShake/ShakeConfig.hs
+++ src/PhotoShake/ShakeConfig.hs
@@ -430,10 +430,12 @@ getDumpFiles config = do
         Dump x -> do
             files <- listDirectory x `catchAny` (\_ -> throw DumpMissing)
             let files' = filter (\z -> isExtensionOf "CR2" z || (isExtensionOf "cr2" z)) files -- bad use
+            let files2' = filter (\z -> isExtensionOf "JPG" z || (isExtensionOf "jpg" z)) files -- bad use
             files'' <- mapM (\file -> do 
                     b <- (doesFileExist (x </> file -<.> "JPG")) 
                     b1 <- (doesFileExist (x </> file -<.> "jpg")) 
-                    if b || b1 then return file else throw JPGMissing ) files'
+                    let b3 = all id $ fmap (\z -> elem (z -<.> "CR2") files' || (elem (z -<.> "cr2") files')) files2'
+                    if ( b || b1 ) && b3 then return file else throw JPGMissing ) files'
             return $ fmap (\y -> (x </> y, x </> y -<.> "JPG")) files'' -- could be nicer
 
 
