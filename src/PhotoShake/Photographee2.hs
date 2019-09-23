cc95592ba392867ee8a8e637b8b42b62f46260c5
Magnus Møller <magnus108@me.com>
2019-09-23 19:28:41 +0000

    gg

--- src/PhotoShake/Photographee2.hs
+++ src/PhotoShake/Photographee2.hs
@@ -12,11 +12,13 @@ module PhotoShake.Photographee2
     , findPhotographee
     ) where
 
+import qualified Data.Vector as Vector
 import Prelude (fromIntegral)
 import qualified PhotoShake.Location as Location
 import qualified PhotoShake.Id as Id
 import Data.Char
 import Control.Monad 
+import Data.Monoid
 import Data.Maybe
 import Data.Either
 import System.IO 
@@ -30,6 +32,7 @@ import Data.Csv
 import PhotoShake.ShakeError
 import Data.List 
 import Data.Function ((.), ($))
+import qualified PhotoShake.Grade as Grade
 
 
 data Photographee 
@@ -75,4 +78,16 @@ findPhotographee location id = do
                         (\i -> fromMaybe noPhotographee (find ((i ==) . _ident ) locData)) id
 
         return studentData
+        ) location
+
+
+fromGrade :: Location.Location -> Grade.Grade -> IO [Photographee]
+fromGrade location grade =
+    Location.location (return []) (\l -> do -- kind of bad
+        locationData' <-  BL.readFile l 
+        let locationData = decodeWith myOptionsDecode NoHeader $ locationData' 
+        let studentData = case locationData of
+                Left _ -> throw ParseLocationFile
+                Right locData -> Vector.filter ((grade ==) . _grade) locData
+        return $ Vector.toList $ studentData
         ) location
